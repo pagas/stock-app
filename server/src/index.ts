073da96857bce6f042391d1215cb8ac2e3493a41
paulius <paulius@2simple.com>
2026-06-09 16:41:18 +0100

    feat: implement Express server with basic API endpoint and JSON middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Server file loaded...");
+import express, { Request, Response } from 'express';
+
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('API is running');
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port 5000');
+});
